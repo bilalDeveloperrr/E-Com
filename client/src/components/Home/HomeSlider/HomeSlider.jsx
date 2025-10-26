164fe7da4c9a461963e35e7d8f7487196345d295
Bilal khan <bilaldev@018@gmail.com>
2025-10-26 06:03:34 +0530

    home slider completed

---
+++ client/src/components/Home/HomeSlider/HomeSlider.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/autoplay";
+
+// Import required modules
+import { Navigation, Autoplay } from "swiper/modules";
+
+const HomeSlider = () => {
+  return (
+    <div className="home-slider mt-3 relative">
+      <div className="container mx-auto px-2">
+        <Swiper
+          navigation={true}
+          spaceBetween={10}
+          modules={[Navigation, Autoplay]}
+          autoplay={{
+            delay: 2500,
+            disableOnInteraction: false,
+          }}
+          loop={true}
+          className="mySwiper custom-swiper"
+          breakpoints={{
+            320: { spaceBetween: 5 },
+            640: { spaceBetween: 10 },
+            1024: { spaceBetween: 20 },
+          }}
+        >
+          {[
+            "https://serviceapi.spicezgold.com/download/1761362043357_34292.jpg",
+            "https://serviceapi.spicezgold.com/download/1761362025223_34295.jpg",
+            "https://serviceapi.spicezgold.com/download/1759938751802_30744.jpg",
+            "https://serviceapi.spicezgold.com/download/1751685130717_NewProject(8).jpg",
+            "https://serviceapi.spicezgold.com/download/1748955932914_NewProject(1).jpg",
+          ].map((src, index) => (
+            <SwiperSlide
+              key={index}
+              className="flex justify-center items-center bg-black/5 rounded-xl"
+            >
+              <img
+                src={src}
+                alt={`Slide ${index + 1}`}
+                className="w-full max-h-[450px] sm:max-h-[350px] md:max-h-[400px] lg:max-h-[500px] object-contain rounded-xl"
+              />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+    </div>
+  );
+};
+
+export default HomeSlider;
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import HomeSlider from '../../components/Home/HomeSlider/HomeSlider'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <HomeSlider/>
+    </div>
   )
 }
 
